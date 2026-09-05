85e7e574c9d794342a9d1bd55b67d90956fb5068
Shashwat Jaiswal <shashwat.jaiswal2025@vitstudent.ac.in>
2026-09-05 10:39:41 +0530

    Added some basic parameters that i will need

--- Functions.py
+++ Functions.py
@@ -1,5 +1,16 @@
 
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.
 
-def NPK_prediction():
+
+def NPK_prediction(Current_soil_data=[6.5, 0.3, 0.2],temp = 25,rainfall = 100,humidity = 60,soil_type = 'loamy'):
     #this is just fot jump start and testing purposese of front end
+    
+    
+
+    
     return [0.5, 0.3, 0.2]
